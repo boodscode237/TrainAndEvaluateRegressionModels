5ba3ccda64e100de892815c4b2855b7e8580b337
boodscode237 <boodscode@gmail.com>
2022-06-30 19:51:03 +0500

    Train a Regression Model

--- main.py
+++ main.py
@@ -1,16 +1,93 @@
-# This is a sample Python script.
+# Explore the Data
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import pandas as pd
+
+# load the training dataset
+data_bikes = 'data.csv'
+bike_data = pd.read_csv(data_bikes)
+print(bike_data.head())
+
+# For example, let's add a new column named day to the dataframe by extracting the day component from the existing
+# dteday column.
+
+bike_data['day'] = pd.DatetimeIndex(bike_data['dteday']).day
+bike_data.head(32)
+print(bike_data.head())
+
+# We can use the dataframe's describe method to generate these for the numeric features as well as the rentals
+# label column.
+numeric_features = ['temp', 'atemp', 'hum', 'windspeed']
+print(bike_data[numeric_features + ['rentals']].describe())
+
+import matplotlib.pyplot as plt
+
+# This ensures plots are displayed inline in the Jupyter notebook
+# %matplotlib inline
+
+# Get the label column
+label = bike_data['rentals']
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# Create a figure for 2 subplots (2 rows, 1 column)
+fig, ax = plt.subplots(2, 1, figsize = (12,12))
+
+# Plot the histogram
+ax[0].hist(label, bins=100)
+ax[0].set_ylabel('Frequency')
+
+# Add lines for the mean, median, and mode
+ax[0].axvline(label.mean(), color='magenta', linestyle='dashed', linewidth=2)
+ax[0].axvline(label.median(), color='cyan', linestyle='dashed', linewidth=2)
+
+# Plot the boxplot
+ax[1].boxplot(label, vert=False)
+ax[1].set_xlabel('Rentals')
+
+# Add a title to the Figure
+fig.suptitle('Rental Distribution')
+
+# Show the figure
+fig.show()
+
+for col in numeric_features:
+    fig = plt.figure(figsize=(9, 6))
+    ax = fig.gca()
+    feature = bike_data[col]
+    feature.hist(bins=100, ax = ax)
+    ax.axvline(feature.mean(), color='magenta', linestyle='dashed', linewidth=2)
+    ax.axvline(feature.median(), color='cyan', linestyle='dashed', linewidth=2)
+    ax.set_title(col)
+plt.show()
+
+# plot a bar plot for each categorical feature count
+categorical_features = ['season','mnth','holiday','weekday','workingday','weathersit', 'day']
+
+for col in categorical_features:
+    counts = bike_data[col].value_counts().sort_index()
+    fig = plt.figure(figsize=(9, 6))
+    ax = fig.gca()
+    counts.plot.bar(ax = ax, color='steelblue')
+    ax.set_title(col + ' counts')
+    ax.set_xlabel(col)
+    ax.set_ylabel("Frequency")
+plt.show()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Now that we know something about the distribution of the data in our columns, we can start to look for relationships
+# between the features and the rentals label we want to be able to predict.
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+for col in numeric_features:
+    fig = plt.figure(figsize=(9, 6))
+    ax = fig.gca()
+    feature = bike_data[col]
+    label = bike_data['rentals']
+    correlation = feature.corr(label)
+    plt.scatter(x=feature, y=label)
+    plt.xlabel(col)
+    plt.ylabel('Bike Rentals')
+    ax.set_title('rentals vs ' + col + '- correlation: ' + str(correlation))
+plt.show()
+
+# Train a Regression Model
+
+
